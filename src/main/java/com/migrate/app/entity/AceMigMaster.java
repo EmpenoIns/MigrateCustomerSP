b126713e5db59c5cdd17c16890931a9a7e463878
Sagar Patil <sagarpatil4323@gmail.com>
2025-05-31 21:34:21 +0530

    Update AceMigMaster.java
    Add import javax.persistence

--- src/main/java/com/migrate/app/entity/AceMigMaster.java
+++ src/main/java/com/migrate/app/entity/AceMigMaster.java
@@ -4,7 +4,10 @@ import java.sql.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
